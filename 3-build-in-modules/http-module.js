682778fdb8e0f1466069059d32e4a94762307ade
DAYAN MANRIQUE <dayanalexismanrique@hotmail.com>
2024-03-10 18:45:20 -0500

    node information

--- 3-build-in-modules/http-module.js
+++ 3-build-in-modules/http-module.js
@@ -1,1 +1,9 @@
 const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") res.end("wellCome to our home page");
+  if (req.url === "/about") res.end("here is our short ");
+  res.end(`<h1>Oops!</h1> <p> not page found </p>`);
+});
+
+server.listen(5000);
